6c8e05a2de9609ec46df49037c5fd10c4b26fc35
Arredondo-Juan <juan_arredondo@icloud.com>
2025-04-05 22:54:39 -0500

    Initial Commit

---
+++ MediaBrowsing/MediaBrowsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaBrowsingApp.swift
+//  MediaBrowsing
+//
+//  Created by Juan Arredondo on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MediaBrowsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
